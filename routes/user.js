bee4b6595fd885453a70f1113a5cb328b9cc8244
TechAkhil <43501564+techakhil-me@users.noreply.github.com>
2022-03-27 01:59:58 +0530

    signin fix

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { registerUser, signInUser } = require('../controllers/user');
+const { registerUser, signInUser } = require("../controllers/user");
 
-router.post('/register', registerUser);
-router.get('/signin', signInUser);
+router.post("/register", registerUser);
+router.post("/signin", signInUser);
 
 module.exports = router;
