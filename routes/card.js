715ba311998f1400cef04a44eebc5db88c92962a
TechAkhil <43501564+techakhil-me@users.noreply.github.com>
2022-03-27 02:26:11 +0530

    get cards fix

--- routes/card.js
+++ routes/card.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { addCard, getCard } = require('../controllers/card');
+const express = require("express");
+const { addCard, getCard } = require("../controllers/card");
 const router = express.Router();
 
-router.post('/addcard', addCard);
-router.get('/getcards', getCard);
+router.post("/addcard", addCard);
+router.post("/getcards", getCard);
 
 module.exports = router;
