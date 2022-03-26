5d81b7ce987ea23e0adc7b9a8a4df3c0630cdc94
TechAkhil <43501564+techakhil-me@users.noreply.github.com>
2022-03-27 01:14:31 +0530

    cors fix

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-const userRoutes = require('./routes/user');
-const offerRoutes = require('./routes/offer');
-const cardRoutes = require('./routes/card');
-
+const userRoutes = require("./routes/user");
+const offerRoutes = require("./routes/offer");
+const cardRoutes = require("./routes/card");
+app.use(
+  cors({
+    origin: "*"
+  })
+);
 app.use(express.json());
 
 // app.use(flash());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-app.use('/user', userRoutes);
-app.use('/offer', offerRoutes);
-app.use('/card', cardRoutes);
+app.use("/user", userRoutes);
+app.use("/offer", offerRoutes);
+app.use("/card", cardRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server Working On Port: ${PORT}`);
